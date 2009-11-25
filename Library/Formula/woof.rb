5179357517ab23d0b381e9d252b31dcc6fc4a26d
Tom Lieber <tom@alltom.com>
2009-11-25 10:46:14 -0800

    okay, don't need those comments

--- Library/Formula/woof.rb
+++ Library/Formula/woof.rb
@@ -6,13 +6,8 @@ class Woof <Formula
   md5 '87d71978789e3444c115048dd686d418'
   version '2009.02.27'
 
-# depends_on 'cmake'
-
   def install
     system "mkdir -p #{prefix}/bin"
     system "cp woof-2009-02-27.py #{prefix}/bin/woof"
-#   system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-#   system "cmake . #{std_cmake_parameters}"
-#   system "make install"
   end
 end
